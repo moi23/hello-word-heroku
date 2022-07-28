4515a1571c5aa73899934609dc18f4ec91532024
Moises Vilas Boas <moises@sistematize.me>
2022-07-28 19:53:33 -0300

    fix: port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-
 const server = express();
-const port = process.env || 3000;
+
+const port = process.env.PORT || 3000;
 
 //server.use(cors());
 server.get("/ping", (request, response) => {
