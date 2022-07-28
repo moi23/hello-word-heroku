8d36304c8e31fb91fb0d0cea28c8ddaa3b9c009e
Moises Vilas Boas <moises@sistematize.me>
2022-07-28 17:29:58 -0300

    feat: all

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const server = express();
+const PORT = 3333;
+
+server.use(cors());
+server.use(express.json());
+
+server.get("/", (request, response) => {
+  return response.json({
+    message: "Success, Hello World Heroku ;D",
+  });
+});
+
+server.listen(PORT, () => console.log("On Fire - Hello Word Heroku :D"));
