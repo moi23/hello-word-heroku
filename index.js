ce05a07cfdd3c1924f88344b5d0951ad4f603f37
Moises Vilas Boas <moises@sistematize.me>
2022-07-28 19:21:55 -0300

    teste

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
+//const cors = require("cors");
 
 const server = express();
 const PORT = 3000;
 
-server.use(cors());
+//server.use(cors());
 server.use(express.json());
 
 server.get("/", (request, response) => {
