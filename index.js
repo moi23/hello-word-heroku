7d55baaaae4d975237e1376f1ed17aaf8f179202
Moises Vilas Boas <moises@sistematize.me>
2022-07-28 19:24:55 -0300

    teste

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 //const cors = require("cors");
 
 const server = express();
-const PORT = 3000;
+const PORT = process.env || 3000;
 
 //server.use(cors());
 server.use(express.json());
